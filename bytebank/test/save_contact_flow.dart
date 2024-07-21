5306c871c91b861e6a8477cfcfeae30202b6c221
Marco <mbarozzi@gmail.com>
2024-07-21 16:00:15 -0300

    uso do pump()

--- bytebank/test/save_contact_flow.dart
+++ bytebank/test/save_contact_flow.dart
@@ -1,22 +1,29 @@
 import 'package:bytebank/main.dart';
+import 'package:bytebank/screens/contacts_list.dart';
 import 'package:bytebank/screens/dashboard.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'matchers.dart';
 
-void main(){
-  testWidgets('Should be save contact',(tester) async{
+void main() {
+  testWidgets('Should be save contact', (tester) async {
     await tester.pumpWidget(BytebankApp());
 
     final dashboard = find.byType(Dashboard);
-    expect(dashboard,findsOneWidget);
+    expect(dashboard, findsOneWidget);
 
-    final transferFeatureItem = find.byWidgetPredicate((widget) =>  
-       featureItemMatcher(widget, 'Transfer', Icons.monetization_on)
-    );
-    expect(transferFeatureItem,findsOneWidget);
+    final transferFeatureItem = find.byWidgetPredicate((widget) =>
+        featureItemMatcher(widget, 'Transfer', Icons.monetization_on));
 
-    
+    expect(transferFeatureItem, findsOneWidget);
+
+    await tester.tap(transferFeatureItem);
+
+    await tester.pump();
+
+    final contactsList = find.byType(ContactsList);
+    expect(contactsList, findsOneWidget);
+
   });
 }
