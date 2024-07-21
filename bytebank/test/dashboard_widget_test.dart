61c87232609050139c8735fa9d9d80279b40b356
Marco <mbarozzi@gmail.com>
2024-07-21 09:05:53 -0300

    otimizando o uso do byWidgetPredicate

--- bytebank/test/dashboard_widget_test.dart
+++ bytebank/test/dashboard_widget_test.dart
@@ -35,26 +35,27 @@ void main(){
   expect(firstFeature, findsWidgets);
 });
 
-// testWidgets('Should display the first feature when the Dashboard is opened',
-//     (tester) async {
-//   await tester.pumpWidget(MaterialApp(home: Dashboard()));
-//   final firstFeature = find.widgetWithIcon(FeatureItem, Icons.monetization_on);
-//   expect(firstFeature, findsWidgets);
 
-//   final nameTransferFeatureItem = find.widgetWithText(FeatureItem, 'Transfer');
-//   expect(nameTransferFeatureItem, findsOneWidget);
-// });
+bool featureItemMatcher(Widget widget, String name, IconData icon) {
+  if(widget is FeatureItem) {
+    return widget.name ==  name && widget.icon == icon;
+  }
+  return false;
+}
 
 testWidgets('Should display the transfer feature when the Dashboard is opened',
-      (tester) async {
-    await tester.pumpWidget(MaterialApp(home: Dashboard()));
-    final transferFeatureItem = find.byWidgetPredicate((widget) {
-      if(widget is FeatureItem) {
-        return widget.name == 'Transfer' && widget.icon == Icons.monetization_on;
-      }
-      return false;
-    });
-    expect(transferFeatureItem, findsOneWidget);
-  });
+    (tester) async {
+  await tester.pumpWidget(MaterialApp(home: Dashboard()));
+  final transferFeatureItem = find.byWidgetPredicate((widget) => featureItemMatcher(widget, 'Transfer', Icons.monetization_on));
+  expect(transferFeatureItem, findsOneWidget);
+});
+testWidgets('Should display the transaction feed feature when the Dashboard is opened',
+    (tester) async {
+  await tester.pumpWidget(MaterialApp(home: Dashboard()));
+  final transactionFeedFeatureItem = find.byWidgetPredicate((widget) => featureItemMatcher(widget, 'Transaction Feed', Icons.description));
+  expect(transactionFeedFeatureItem, findsOneWidget); 
+}); 
+
+
 
 }
